0233eaf5a2af0fcdb3d01a2e696030b93097bb54
shin-tc-10 <shin.nkt.09@gmail.com>
2023-10-09 22:44:58 +0900

    検索機能実装

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 function App() {
   const [todo, setTodo] = useState("");
   const [newTodo, setNewTodo] = useState([]);
+  const [searchTodo, setSearchTodo] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -25,6 +26,12 @@ function App() {
     setNewTodo(newTodo.filter((oldTodo) => oldTodo !== todo));
   }
 
+  const handleSearchKeyword = (e) => {
+    e.preventDefault();
+    setSearchTodo(newTodo.filter((oldTodo) => oldTodo == e.target.value));
+    console.log(searchTodo);
+  }
+
   return (
     <div className="App">
       <h1>React First Todo</h1>
@@ -33,18 +40,31 @@ function App() {
         <input type="text" placeholder='New Todo' value={todo} onChange={handleTodo}></input>
         <button type="submit">ADD</button>
       </form>
-      <input type="text" placeholder='Search Keyword'></input>
-      <ul>{newTodo.map((todo) => {
-        return (
-          <div key={todo}>
-            <li>{todo}</li>
-            <button onClick={() => { handleDelete(todo) }}>削除</button>
-          </div>
-        )
+      <input type="text" placeholder='Search Keyword' onChange={handleSearchKeyword}></input>
 
-      })}
+      <ul>{searchTodo != "" ?
+        <> {searchTodo.map((todo, index) => {
+          return (
+            <div key={todo + index}>
+              <li>{todo}</li>
+              <button onClick={() => { handleDelete(todo) }}>削除</button>
+            </div>
+          )
+        })}
+        </> :
+        <>{
+          newTodo.map((todo, index) => {
+            return (
+              <div key={todo + index}>
+                <li>{todo}</li>
+                <button onClick={() => { handleDelete(todo) }}>削除</button>
+              </div>
+            )
+
+          })}</>
+      }
       </ul>
-    </div>
+    </div >
   );
 }
 
