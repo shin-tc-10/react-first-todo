b7b23c8efa511d897590dd93e03dbec3609637cb
shin-tc-10 <shin.nkt.09@gmail.com>
2023-10-09 22:23:09 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [newTodo, setNewTodo] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todo == "") {
+      return;
+    }
+    setNewTodo([...newTodo, todo]);
+    console.log(newTodo);
+    setTodo("");
+  }
+
+  const handleTodo = (e) => {
+    setTodo(e.target.value);
+  }
+
+  const handleDelete = (todo) => {
+    console.log(todo);
+    setNewTodo(newTodo.filter((oldTodo) => oldTodo !== todo));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React First Todo</h1>
+      <p>Add Todo</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='New Todo' value={todo} onChange={handleTodo}></input>
+        <button type="submit">ADD</button>
+      </form>
+      <input type="text" placeholder='Search Keyword'></input>
+      <ul>{newTodo.map((todo) => {
+        return (
+          <div key={todo}>
+            <li>{todo}</li>
+            <button onClick={() => { handleDelete(todo) }}>削除</button>
+          </div>
+        )
+
+      })}
+      </ul>
     </div>
   );
 }
