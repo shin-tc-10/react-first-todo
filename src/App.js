852a9f177e02b9c43e65e1a293c3dc5e40acb754
shin-tc-10 <shin.nkt.09@gmail.com>
2023-10-12 21:16:49 +0900

    カスタムフック化

--- src/App.js
+++ src/App.js
@@ -1,45 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState } from "react";
+import useTodo from './hooks/useTodo';
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [newTodo, setNewTodo] = useState([]);
-  const [searchTodo, setSearchTodo] = useState([]);
-  const [flag, setFlag] = useState(false);
-
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (todo == "") {
-      return;
-    }
-    setNewTodo([...newTodo, todo]);
-    setTodo("");
-  }
-
-  const handleTodo = (e) => {
-    setTodo(e.target.value);
-  }
-
-  const handleDelete = (todo) => {
-    setNewTodo(newTodo.filter((oldTodo) => oldTodo !== todo));
-  }
-
-  const handleSearchKeyword = (e) => {
-    e.preventDefault();
-    if (e.target.value == "") {
-      setSearchTodo(newTodo);
-      setFlag(false);
-      return;
-    }
-    const keyword = e.target.value;
-    const searchKeyword = keyword.trim().toLowerCase().match(/[^\s]+/g);
-    let result = newTodo.filter((todo) => todo.startsWith(searchKeyword));
-    console.log(result);
-    setSearchTodo(result);
-    setFlag(true);
-  }
+  const { handleSubmit, handleTodo, handleDelete, handleSearchKeyword, flag, todo, searchTodo, newTodo } = useTodo();
 
   return (
     <div className="App">
---
+++ src/hooks/useTodo.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+const useTodo = () => {
+    const [todo, setTodo] = useState("");
+  const [newTodo, setNewTodo] = useState([]);
+  const [searchTodo, setSearchTodo] = useState([]);
+  const [flag, setFlag] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todo == "") {
+      return;
+    }
+    setNewTodo([...newTodo, todo]);
+    setTodo("");
+  }
+
+  const handleTodo = (e) => {
+    setTodo(e.target.value);
+  }
+
+  const handleDelete = (todo) => {
+    setNewTodo(newTodo.filter((oldTodo) => oldTodo !== todo));
+  }
+
+  const handleSearchKeyword = (e) => {
+    e.preventDefault();
+    if (e.target.value == "") {
+      setSearchTodo(newTodo);
+      setFlag(false);
+      return;
+    }
+    const keyword = e.target.value;
+    const searchKeyword = keyword.trim().toLowerCase().match(/[^\s]+/g);
+    let result = newTodo.filter((todo) => todo.startsWith(searchKeyword));
+    console.log(result);
+    setSearchTodo(result);
+    setFlag(true);
+  }
+
+
+  return {handleSubmit: handleSubmit, handleTodo: handleTodo, handleDelete: handleDelete, handleSearchKeyword: handleSearchKeyword, todo: todo, newTodo: newTodo, searchTodo: searchTodo, flag: flag};
+}
+
+export default useTodo
