c8201cea392869a55c30daae7d38ec14822c8620
shin-tc-10 <shin.nkt.09@gmail.com>
2023-11-13 23:14:08 +0900

    jestを使ったテスト実施

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Test Login Component', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = await screen.getByText("React Todo List");
+  expect(text).toBeInTheDocument();
 });
