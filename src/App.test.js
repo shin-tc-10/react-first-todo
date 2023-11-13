09436ee84bb56792eeb0e85fce34bc5aa9cbc5e8
shin-tc-10 <shin.nkt.09@gmail.com>
2023-11-13 23:16:00 +0900

    jestを使ったテスト実施

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Test Login Component', async () => {
-  render(<App />);
-  const text = await screen.getByText("React Todo List");
-  expect(text).toBeInTheDocument();
-});
+describe("Test App Component", () => {
+  test('render form with React Todo List text', async () => {
+    render(<App />);
+    const text = await screen.getByText("React Todo List");
+    expect(text).toBeInTheDocument();
+  })
+})
